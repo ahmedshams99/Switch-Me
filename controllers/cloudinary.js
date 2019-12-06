e7ed765e58b48a7cae4881adc861c6dbd26eaafc
B1aCkManTa <mohamed.mantawy@student.guc.edu.eg>
2019-12-06 18:29:10 +0200

    cloudinary upload image

--- controllers/cloudinary.js
+++ controllers/cloudinary.js
@@ -1,10 +1,11 @@
 var cloudinary = require('cloudinary').v2;
-const cloudinaryConfig =require("../config/keys");
+const cloudinaryConfig = require("../config/keys");
 
 cloudinary.config(cloudinaryConfig.cloudinary.cloudinaryData);
 
+
 exports.upload = async function(req,res){
-    const file = req.files.photo;
+    const file = req.files;
     cloudinary.uploader.upload(file.tempFilePath,function(err,result){
         if(err){
             return err
